e244fe9c7508a0fa6c7ebb9ce13c21e2fa085fbd
RyoLeee <h4285007@gmail.com>
2026-07-31 22:32:11 +0700

    Update auth.api

--- pokemon_machine/app/api/auth_api.py
+++ pokemon_machine/app/api/auth_api.py
@@ -1,23 +1,25 @@
 from fastapi import FastAPI, Depends, HTTPException
+from pokemon_machine.app.db import session
 from sqlalchemy.orm import Session
 from database import get_db
 import crud
 from schemas.pokemon_owner import PokemonOwnerCreate, PokemonOwnerLogin
+from app.controller.auth_services import createIdentityHandle, loginHandle
 
 app = FastAPI()
 
 @app.post("/auth/register")
 async def create_identity(user_data: PokemonOwnerCreate, db: Session = Depends(get_db)):
-    result = 
+    result = await createIdentityHandle(user_data, db)
     return result
 
 @app.post("/auth/login")
 async def login(user_data: PokemonOwnerLogin, db: Session = Depends(get_db)):
-    result:
+    result = await loginHandle(user_data, db)
     return result
 
 @app.post('/auth/logout')
 async def logout():
-    result:
+    result = 
     return result
 
